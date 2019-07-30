0aae78e4d77fa7bc28c99ca30f6e301e3e4514bc
Albert Yu <albertyu@Alberts-Air.attlocal.net>
2019-07-29 22:19:21 -0700

    first table

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tech from "./components/technology";
+
+
+// const App = () => <Tech />;
 
 class App extends Component {
   render() {
@@ -8,11 +12,18 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
           <h2>HEllO People</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <input></input>
+          <button> Search </button>
+          
+        </div> 
+        
+        <Tech />
+
+
+
+        
       </div>
     );
   }
---
+++ client/src/components/technology/technology.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+// import "./technology.css"
+
+const Tech = () => (
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Technology</th>
+      <th scope="col">P/R</th>
+      <th scope="col">P/E</th>
+      <th scope="col">PT</th>
+      <th scope="col">200MA</th>
+      <th scope="col">50MA</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+);
+
+export default Tech;
