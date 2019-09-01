87f17d407dd018821ee06bcab8c665e16257afe4
Albert Yu <albertyu@Alberts-MacBook-Air.local>
2019-08-31 18:40:18 -0700

    bunched api promises

--- client/src/components/sector_1/sector_1.js
+++ client/src/components/sector_1/sector_1.js
@@ -3,7 +3,6 @@ import "./sector_1.css";
 import DataTable from 'react-data-table-component';
 import API from "../../utils/API";
 
-
 const columns = [
   {
     name: 'Market',
@@ -17,7 +16,7 @@ const columns = [
     right: true,
   },
   {
-    name: 'P/R',
+    name: 'Price to Sales(MC/Annual Sales)',
     selector: 'pr',
     sortable: true,
     right: true,
@@ -41,23 +40,29 @@ const columns = [
     right: true,
   },
   {
-    name: 'Rev Gro1',
-    selector: 'rg1',
+    name: 'Others PT',
+    selector: 'opt',
     sortable: true,
     right: true,
   },
-  {
-    name: 'Rev Gro2',
-    selector: 'rg2',
-    sortable: true,
-    right: true,
-  },
-  {
-    name: 'Rev Gro3',
-    selector: 'rg3',
-    sortable: true,
-    right: true,
-  },
+  // {
+  //   name: 'Rev Gro1',
+  //   selector: 'rg1',
+  //   sortable: true,
+  //   right: true,
+  // },
+  // {
+  //   name: 'Rev Gro2',
+  //   selector: 'rg2',
+  //   sortable: true,
+  //   right: true,
+  // },
+  // {
+  //   name: 'Rev Gro3',
+  //   selector: 'rg3',
+  //   sortable: true,
+  //   right: true,
+  // },
   {
     name: 'DCF PT',
     selector: 'dcfpt',
@@ -91,206 +96,117 @@ class S1 extends Component {
 
 componentDidMount() {
   //call database for the stocks
-const stocksearch = ['RTN', 'LMT', 'NOC', 'BA', 'FDX']
-//first stock on list. Have API.js make calls through watchlist in
-  API.getQuote(stocksearch[0])
-  .then(res => {
 
+
+API.getQuote('RTN', 'LMT', 'NOC', 'BA', 'FDX').then(res => {
+  // console.log(res)
+  
   //calculate price targets 
   //price to earnings
   const PT1 = 123
-
   //price to revenue
   const PT2 = 13141
-
   //DCF
   const PT3 = 3131
 
   const PT = (PT1 + PT2 + PT3)/3
-
-    // console.log(res.data)
-
-  API.getRevenue(stocksearch[0]).then(res2 =>{
-      // console.log(res2.data)
-    this.setState({
+  //setState
+  this.setState({
         tableData: [{
-          market: res.data.symbol, 
-          pe: res.data.peRatio, 
-          pr: 'api call', 
-          profit: (res2.data.cashflow[0].netIncome)/1000000, 
-          marketcap: (res.data.marketCap/1000000000),
-          pt: PT,
-          rg1: 'api call',
-          rg2: 'api call',
-          rg3: 'api call',
+          market: res[0].data.symbol, 
+          pe: res[0].data.peRatio, 
+          pr: (res[0].data.marketCap)/(res[10].data), 
+          profit: res[5].data.cashflow[0].netIncome/1000000, 
+          marketcap: res[0].data.marketCap/1000000000,
+          pt: 0,
+          opt: 0,
+          // rg1: 'api call',
+          // rg2: 'api call',
+          // rg3: 'api call',
           dcfpt: 'calc',
           prpt: 'calc',
           pept: 'calc'
         }]
       })
-    })
-  });
 
-
-  API.getQuote(stocksearch[1])
-  .then(res => {
-
-  //calculate price targets 
-  //price to earnings
-  const PT1 = 123
-
-  //price to revenue
-  const PT2 = 13141
-
-  //DCF
-  const PT3 = 3131
-
-  const PT = (PT1 + PT2 + PT3)/3
-
-    // console.log(res.data)
-
-  API.getRevenue(stocksearch[1]).then(res2 =>{
-      // console.log(res2.data)
-    this.setState({
+  this.setState({
         tableData: [...this.state.tableData, 
         {
-          market: res.data.symbol, 
-          pe: res.data.peRatio, 
-          pr: 'api call', 
-          profit: (res2.data.cashflow[0].netIncome)/1000000, 
-          marketcap: (res.data.marketCap/1000000000),
-          pt: PT,
-          rg1: 'api call',
-          rg2: 'api call',
-          rg3: 'api call',
+          market: res[1].data.symbol, 
+          pe: res[1].data.peRatio, 
+          pr: (res[1].data.marketCap)/(res[11].data), 
+          profit: res[6].data.cashflow[0].netIncome/1000000, 
+          marketcap: res[1].data.marketCap/1000000000,
+          pt: 0,
+          opt: 0,
+          // rg1: 'api call',
+          // rg2: 'api call',
+          // rg3: 'api call',
           dcfpt: 'calc',
           prpt: 'calc',
           pept: 'calc'
-
         }]
       })
-    })
-  })
 
-  API.getQuote(stocksearch[2])
-  .then(res => {
-
-  //calculate price targets 
-  //price to earnings
-  const PT1 = 123
-
-  //price to revenue
-  const PT2 = 13141
-
-  //DCF
-  const PT3 = 3131
-
-  const PT = (PT1 + PT2 + PT3)/3
-
-    // console.log(res.data)
-
-  API.getRevenue(stocksearch[2]).then(res2 =>{
-      // console.log(res2.data)
-    this.setState({
+  this.setState({
         tableData: [...this.state.tableData, 
         {
-          market: res.data.symbol, 
-          pe: res.data.peRatio, 
-          pr: 'api call', 
-          profit: (res2.data.cashflow[0].netIncome)/1000000, 
-          marketcap: (res.data.marketCap/1000000000),
-          pt: PT,
-          rg1: 'api call',
-          rg2: 'api call',
-          rg3: 'api call',
+          market: res[2].data.symbol, 
+          pe: res[2].data.peRatio, 
+          pr: (res[2].data.marketCap)/(res[12].data), 
+          profit: res[7].data.cashflow[0].netIncome/1000000, 
+          marketcap: res[2].data.marketCap/1000000000,
+          pt: 0,
+          opt: 0,
+          // rg1: 'api call',
+          // rg2: 'api call',
+          // rg3: 'api call',
           dcfpt: 'calc',
           prpt: 'calc',
           pept: 'calc'
+
         }]
       })
-    })
-  })
 
-API.getQuote(stocksearch[3])
-  .then(res => {
-
-  //calculate price targets 
-  //price to earnings
-  const PT1 = 123
-
-  //price to revenue
-  const PT2 = 13141
-
-  //DCF
-  const PT3 = 3131
-
-  const PT = (PT1 + PT2 + PT3)/3
-
-    // console.log(res.data)
-
-  API.getRevenue(stocksearch[3]).then(res2 =>{
-      // console.log(res2.data)
-    this.setState({
+  this.setState({
         tableData: [...this.state.tableData, 
         {
-          market: res.data.symbol, 
-          pe: res.data.peRatio, 
-          pr: 'api call', 
-          profit: (res2.data.cashflow[0].netIncome)/1000000, 
-          marketcap: (res.data.marketCap/1000000000),
-          pt: PT,
-          rg1: 'api call',
-          rg2: 'api call',
-          rg3: 'api call',
+          market: res[3].data.symbol, 
+          pe: res[3].data.peRatio, 
+          pr: (res[3].data.marketCap)/(res[13].data), 
+          profit: res[8].data.cashflow[0].netIncome/1000000, 
+          marketcap: res[3].data.marketCap/1000000000,
+          pt: 0,
+          opt: 0,
+          // rg1: 'api call',
+          // rg2: 'api call',
+          // rg3: 'api call',
           dcfpt: 'calc',
           prpt: 'calc',
           pept: 'calc'
+
         }]
       })
-    })
-  })
 
-
-  API.getQuote(stocksearch[4])
-  .then(res => {
-
-  //calculate price targets 
-  //price to earnings
-  const PT1 = 123
-
-  //price to revenue
-  const PT2 = 13141
-
-  //DCF
-  const PT3 = 3131
-
-  const PT = (PT1 + PT2 + PT3)/3
-
-    // console.log(res.data)
-
-  API.getRevenue(stocksearch[4]).then(res2 =>{
-      // console.log(res2.data)
-    this.setState({
+  this.setState({
         tableData: [...this.state.tableData, 
         {
-          market: res.data.symbol, 
-          pe: res.data.peRatio, 
-          pr: 'api call', 
-          profit: (res2.data.cashflow[0].netIncome)/1000000, 
-          marketcap: (res.data.marketCap/1000000000),
-          pt: PT,
-          rg1: 'api call',
-          rg2: 'api call',
-          rg3: 'api call',
+          market: res[4].data.symbol, 
+          pe: res[4].data.peRatio, 
+          pr: (res[4].data.marketCap)/(res[14].data), 
+          profit: res[9].data.cashflow[0].netIncome/1000000, 
+          marketcap: res[4].data.marketCap/1000000000,
+          pt: 0,
+          opt: 0,
+          // rg1: 'api call',
+          // rg2: 'api call',
+          // rg3: 'api call',
           dcfpt: 'calc',
           prpt: 'calc',
           pept: 'calc'
         }]
       })
-    })
   })
-
-
 
 }
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,18 +1,57 @@
 import axios from "axios";
 // const BASEURL = "https://cloud.iexapis.com";
-const APIKEY = "pk_51bdaef9e7324c8f80fa7f6bbe14323a";
-
-//grab watchlist from database.
-//if sector = industrials, show on list
-
+// const APIKEY = "pk_51bdaef9e7324c8f80fa7f6bbe14323a";
 
 
 export default {
-  getQuote: function(query) {
-    return axios.get("https://cloud.iexapis.com/stable/stock/" +  query + "/quote?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a");
-  },
-  getRevenue: function(query) {
-    return axios.get("https://cloud.iexapis.com/stable/stock/" + query + "/cash-flow?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a");
+  getQuote: function(a,b,c,d,e) {
+
+  	//api call #1 for quote data
+  	let stockquote1 = "https://cloud.iexapis.com/stable/stock/" + a + "/quote?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockquote2 = "https://cloud.iexapis.com/stable/stock/" + b + "/quote?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockquote3 = "https://cloud.iexapis.com/stable/stock/" + c + "/quote?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockquote4 = "https://cloud.iexapis.com/stable/stock/" + d + "/quote?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockquote5 = "https://cloud.iexapis.com/stable/stock/" + e + "/quote?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	//api call #2 for cash flow data
+  	let stockcf1 = "https://cloud.iexapis.com/stable/stock/" + a + "/cash-flow?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockcf2 = "https://cloud.iexapis.com/stable/stock/" + b + "/cash-flow?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockcf3 = "https://cloud.iexapis.com/stable/stock/" + c + "/cash-flow?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockcf4 = "https://cloud.iexapis.com/stable/stock/" + d + "/cash-flow?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockcf5 = "https://cloud.iexapis.com/stable/stock/" + e + "/cash-flow?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	//api call #3 for revenue data. this is annual revenue
+  	let stockrev1 = "https://cloud.iexapis.com/stable/data-points/" + a + "/REVENUE?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockrev2 = "https://cloud.iexapis.com/stable/data-points/" + b + "/REVENUE?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockrev3 = "https://cloud.iexapis.com/stable/data-points/" + c + "/REVENUE?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockrev4 = "https://cloud.iexapis.com/stable/data-points/" + d + "/REVENUE?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+  	let stockrev5 = "https://cloud.iexapis.com/stable/data-points/" + e + "/REVENUE?token=pk_51bdaef9e7324c8f80fa7f6bbe14323a"
+
+  	const promise1 = axios.get(stockquote1)
+  	const promise2 = axios.get(stockquote2)
+  	const promise3 = axios.get(stockquote3)
+  	const promise4 = axios.get(stockquote4)
+  	const promise5 = axios.get(stockquote5)
+  	const promise6 = axios.get(stockcf1)
+  	const promise7 = axios.get(stockcf2)
+  	const promise8 = axios.get(stockcf3)
+  	const promise9 = axios.get(stockcf4)
+  	const promise10 = axios.get(stockcf5)
+  	const promise11 = axios.get(stockrev1)
+  	const promise12 = axios.get(stockrev2)
+  	const promise13 = axios.get(stockrev3)
+  	const promise14 = axios.get(stockrev4)
+  	const promise15 = axios.get(stockrev5)
+
+  	return Promise.all([promise1, promise2, promise3, promise4, promise5, 
+  						promise6, promise7, promise8, promise9, promise10,
+  						promise11, promise12, promise13, promise14, promise15
+  	]).then(function(stockarray) {
+  	return stockarray
+  	
+	})
+	.catch(function (err) {
+		console.log(err);
+	})
+    
   }
 
 };
